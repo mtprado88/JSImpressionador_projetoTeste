c4acc0efbbb1119e88530d0595b2c0138b6f3f9d
Marco Tulio <mtpradoti@gmail.com>
2024-01-09 08:59:40 -0300

    Atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ import {turboLogger} from "./turboLogger.js";
 
 console.log ('Estou usando o github');
 turboLogger("estou aprendendo o fluxo do git","red");
+turboLogger("Eu consegui criar uma nova mensagem sem afetar a versao principal do sistema","darkgreen")
